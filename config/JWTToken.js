9ecc37457e990fad482af69792bdd7fa5c305644
AbdoTarek2211 <abdo_bemo@hotmail.com>
2024-04-24 13:52:50 +0200

    Remove Files

--- config/JWTToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-const generateToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SecretKey,{expiresIn : "1d"});
-};
-module.exports = generateToken;
--- utils/validateDB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const validateID = (id =>{
-    const isValid = mongoose.Types.ObjectId.isValid(id);
-    if(!isValid){
-        throw new Error("This ID is not Valid");
-    }
-});
-module.exports = validateID;
