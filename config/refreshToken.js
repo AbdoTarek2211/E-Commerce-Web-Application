eef7b989c52b9340a99f5f498e2151d9f565bc36
Abdelrahman Tarek <abdo_bemo@hotmail.com>
2024-04-25 20:26:43 +0200

    Delete config directory

--- config/refreshToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-const genRefreshToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SecretKey,{expiresIn : "3d"});
-};
-module.exports = genRefreshToken;
